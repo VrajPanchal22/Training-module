9406eca5e65d794de04313676264197fb652298c
Jay Patel <jay@cloudmatetechnologies.com>
2022-12-30 17:26:38 +0530

    pokemon task completed 95%

--- JayPatel/Pokemon/script.js
+++ JayPatel/Pokemon/script.js
@@ -1,25 +1,38 @@
-// const imgUrl = "http://192.168.29.118:3004/images/pokemon/other/home/" //3.png"
+// global variables
 
 let list = [];
 let mainObj = {};
-// let offset = sessionStorage.getItem("offset") ? parseInt(sessionStorage.getItem("offset")) : 1;
-let offset = 1;
 
-main()
+let pageNo = getSessionStorageData("pageNo", 1)
+let pageLimit = 6
+
+let limit = 20
+let offset = (limit * (pageNo - 1)) + 1
+
+let loading = undefined;
+
+let count = getSessionStorageData("count", 0) 
+let endPageNo = Math.ceil(count / limit);
+
+
+
+// -------------------------------main function-------------------------
+
 async function main() {
-    list = sessionStorage.getItem("pokemons") ? JSON.parse(sessionStorage.getItem("pokemons")) : [];
+    list = getSessionStorageData("pokemons", [])
 
     try {
 
+        loadingStart();
+
+
         if (checkExist()) {
-            console.log("hello");
-            console.log("data = ", list.slice(offset, offset+20));
 
-            // set prev and next visible or not
             showButtons();
+            loadingEnd();
+            loadPageNo()
 
-            loadPokemons(list.slice(offset, offset + 20))
-            
+            loadPokemons(list.slice(offset, offset + limit))
             return
         }
 
@@ -28,16 +41,24 @@ async function main() {
         console.log("mainObj = ", mainObj);
         console.log("offset = ", offset);
 
+        // update count
+        count = mainObj.count;
+        endPageNo = Math.ceil(count / limit)
+        
+        sessionStorage.setItem("count", mainObj.count)
+
+        loadPageNo()
+
 
         // set prev and next visible or not
         showButtons();
 
 
         // get all pokemons one by one
-        Promise.all(mainObj.results.map(async (element) => {
-            const pokemon = await getPokemon(element.url)
-            // console.log("p = ", pokemon);
+        const responce = await Promise.all(mainObj.results.map(async (element) => {
 
+            const pokemon = await getPokemon(element.url)
+            
             const pokemonObj = {
                 id: pokemon.id,
                 name: pokemon.name,
@@ -52,15 +73,12 @@ async function main() {
 
             return pokemonObj;
         }))
-            .then(res => {
-                // console.log("list = ", list);
-                console.log("res = ", res);
 
-                sessionStorage.setItem("pokemons", JSON.stringify(list))
+        sessionStorage.setItem("pokemons", JSON.stringify(list))
 
-                // load pokemons
-                loadPokemons(res)
-            })
+        // load pokemons
+        loadingEnd()
+        loadPokemons(responce)
 
     }
     catch (err) {
@@ -68,15 +86,13 @@ async function main() {
     }
 }
 
+main()
 
 
 async function getPokemons() {
 
-    // to save sessionStorage full
-    // sessionStorage.clear()
-
     try {
-        const responce = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset-1}&limit=20`)
+        const responce = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset - 1}&limit=${limit}`)
         // console.log("res = ", responce);
 
         const jsonRes = await responce.json();
@@ -126,15 +142,14 @@ function loadPokemons(pokemons) {
         </a>
     `
     });
-    // document.getElementById("pokemons").innerHTML += 
 }
 
 
 
 
 function checkExist() {
-    for (let i = offset; i < offset + 20; i++) {
-        // console.log("data = ", list[i]);
+    for (let i = offset; i < offset + limit; i++) {
+        // console.log("data[", i, "] = ", list[i]);
 
         if (!list[i]) {
             return false
@@ -148,34 +163,134 @@ function checkExist() {
 // ----------------------buttons------------------------------
 
 function showButtons() {
-    let button = document.getElementById("buttons");
+    let prevButton = document.getElementById("prev");
+    let nextButton = document.getElementById("next");
 
-    if (offset == 1) {
-        button.children[0].classList.add("button--invisible")
+    if (pageNo == 1) {
+        prevButton.classList.add("button--invisible")
     }
     else {
-        button.children[0].classList.remove("button--invisible")
+        prevButton.classList.remove("button--invisible")
+    }
+
+    if (pageNo == endPageNo) {
+        nextButton.classList.add("button--invisible")
+    }
+    else {
+        nextButton.classList.remove("button--invisible")
     }
 
 }
 
-document.getElementById("buttons").addEventListener("click", (event) => {
 
-    if (event.target.id != "buttons") {
-        console.log(event.target);
-        let text = event.target.innerText;
 
-        if (text == "next >") {
-            console.log("next");
-            offset += 20
-            sessionStorage.setItem("offset", "" + offset)
-            main();
-        }
-        else {
-            console.log("prev");
-            offset -= 20
-            sessionStorage.setItem("offset", "" + offset)
-            main();
-        }
+function nextClicked() {
+    offset += limit
+    pageNo += 1
+
+    sessionStorage.setItem("pageNo", "" + pageNo)
+    main();
+    loadPageNo()
+}
+
+function prevClicked() {
+    offset -= limit
+    pageNo -= 1
+
+    sessionStorage.setItem("pageNo", "" + pageNo)
+    main();
+    loadPageNo()
+}
+
+
+
+// ---------------------------pagging-----------------------------
+
+function loadPageNo() {
+
+    pageObj = document.getElementById("pages")
+
+    pageObj.innerHTML = ""
+
+    let start = (pageNo <= 3) ? 1 : pageNo - 2;
+    let end = (pageNo >= endPageNo - 3) ? endPageNo : pageNo + pageLimit - 2
+
+    for (let i = start; i <= end; i++) {
+
+        pageObj.innerHTML += `
+            <span class="pages__num ${i == pageNo ? "pages__num--selected" : ""}" id="${i}">
+                ${(i != 1 && i == pageNo - 2) ? ". . ." : ""}  ${i}  ${i == (pageNo + pageLimit - 2) ? ". . ." : ""}
+            </span>
+        `
+    }
+
+}
+
+document.getElementById("pages").addEventListener("click", (event) => {
+
+    if (event.target.id != "pages") {
+
+        // console.log(event.target.id);
+        let id = parseInt(event.target.id)
+
+        offset = (limit * id) - limit + 1
+        // console.log("offset = ", offset);
+
+        pageNo = id
+        console.log("Page no = ", pageNo);
+
+        sessionStorage.setItem("pageNo", "" + pageNo)
+
+        loadPageNo()
+        main()
+
     }
 })
+
+
+
+
+// -----------------------loading----------------------------------
+
+function loadingStart() {
+    const load = document.getElementById("loaders")
+    let i = 0;
+    
+    let length = load.children.length
+
+    loading = setInterval(() => {
+        for (let index = 0; index < length; index++) {
+            
+            if (index == i) {
+                load.children[index].classList.add("loader--size")
+            }
+            else {
+                load.children[index].classList.remove("loader--size")
+            }
+        }
+
+        i = (i + 1) % length;
+
+    }, 100);
+
+    document.getElementById("pokemons").style.display = 'none'
+    document.getElementById("loaders").style.display = 'flex'
+}
+
+
+function loadingEnd() {
+    // setTimeout(() => {
+    clearInterval(loading)
+    document.getElementById("pokemons").style.display = 'grid'
+    document.getElementById("loaders").style.display = 'none'
+    // }, 1000);
+}
+
+
+
+
+// ------------------general functions-------------------------
+
+function getSessionStorageData(key, defaultValue = "") {
+    return sessionStorage.getItem(key) ? JSON.parse(sessionStorage.getItem(key)) : defaultValue
+}
