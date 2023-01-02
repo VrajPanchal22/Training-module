4868b1c64753672741a4ad0403dd778bf013f055
Vraj <vraj@cloudmatetechnologies@gmail.com>
2023-01-02 14:03:09 +0530

    Pokedex Website

--- Vraj/Javascript-Task/Pokemon/pokemoneStats.js
+++ Vraj/Javascript-Task/Pokemon/pokemoneStats.js
@@ -1,11 +1,10 @@
 id = window.location.search
 console.log(id)
-newid = id.slice(4,7)
+newid = id.slice(4)
 console.log(newid)
 
 const pokemone = JSON.parse(sessionStorage.getItem("" + newid))
-// console.log(pokemone.name, pokemone.height)
-//     console.log(pokemone)   
+   
 
 imgSrc = `http://192.168.29.118:3004/images/pokemon/other/home/${newid}.png`
 console.log(imgSrc)
@@ -16,23 +15,12 @@ document.getElementById('container').innerHTML=`
 </div>
 <div class="content">
  <p> Id: ${pokemone.id}</p>
-<p> Name: ${pokemone.name}</p>
+<p style="text-transform:capitalize;"> Name: ${pokemone.name}</p>
 <p> Height: ${pokemone.height}</p>
 <p> Wight: ${pokemone.weight}</p>
-<p> Abilities: ${pokemone.abilities[0].ability.name}</p>
+<p style="text-transform:capitalize;"> Abilities: ${pokemone.abilities[0].ability.name}</p>
 </div>
 `
-
-
-
-
-
-// document.getElementById("pokemons").innerText="hello"
-// document.querySelector("pokemons").innerHTML = `
-// <div>
-// <img class="pokemon__image" src="${imgSrc}" alt="pokemon">
-// </div>
-// `
 
 
 
