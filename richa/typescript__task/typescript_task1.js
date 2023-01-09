c0b41c0aec2c52ca541c6b3f062d0e4b5705fb22
richact <richa@cloudmatetechnologies.com>
2023-01-09 18:14:27 +0530

    interface_task

---
+++ richa/typescript__task/typescript_task1.js
@@ -0,0 +1,57 @@
+var a = {
+    cardData: {},
+    cardLayout: {
+        body: [
+            {
+                id: "000t1",
+                tileComponent: [],
+                tileType: "WRAP",
+                uiAction: ""
+            },
+        ],
+        footer: {
+            menu: [
+                {
+                    id: "EN01BottomButtonBack",
+                    isSearchable: false,
+                    subView: [
+                        {
+                            title: {
+                                text: ""
+                            }
+                        },
+                    ],
+                    order: 1,
+                    type: "BACK_BUTTON",
+                    uiAction: "${action2}"
+                },
+                {
+                    id: "EN01BottomButton1",
+                    isSearchable: false,
+                    subView: [
+                        {
+                            title: {
+                                text: "00f1v1"
+                            }
+                        },
+                    ],
+                    order: 2,
+                    type: "BUTTON",
+                    uiAction: "action1"
+                },
+            ],
+            orientation: "HORIZONTAL"
+        }
+    },
+    cardUIAction: {
+        action1: {
+            action: "ev-4-patient-ask-for-appointment"
+        },
+        action2: {
+            action: "ev-25-patient-in-touch-navigate-back"
+        }
+    },
+    id: "00jc2",
+    name: "Patient Score Card"
+};
+console.log(a);
